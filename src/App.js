62110f454afa6f56585ca14f7955d0eddcd058da
Pedro Lucas Xavier <pedrolucas.xs@gmail.com>
2022-07-06 15:06:45 -0300

    style: toast com wade criado na App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Wade from './components/Wade';
+import Wade from "./components/Wade";
 import GlobalStyle from "./styles/GlobalStyle";
+import { ToastNotify } from "./components/Toasts";
+import { toast } from "react-toastify";
 
 function App() {
+
+  // function 'notify' pode ser passada por props para os componentes que necesistem dela. Deve ser informada uma mensagem no parametro e o tempo em milissigundos para a animação permanecer na tela.
+
+  const notify = (message, time) => {
+    toast(message, {
+      icon: <Wade width="5rem" height="6rem"/>,
+      autoClose: time
+    });
+  }
+
   return (
     <>
-    <GlobalStyle/>
-    <Wade width='6rem' height='9.5rem'/>
+      <GlobalStyle />
+      <ToastNotify/>
+      {/* Button para testes da notificação */}
+      <button onClick={()=> notify("Olá! Que bom te ver novamente.", 3000)}>Aqui</button>
     </>
   );
 }
