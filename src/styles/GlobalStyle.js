205c0e8d26dd15134d1eecc405199a4cb8ad6b1d
Gustavo Costa <gustavocosta.realitydeveloper@gmail.com>
2022-07-06 11:12:35 -0400

    style: Global updated, Title and baseTypograph added

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,12 +2,30 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle` 
 
+    :root{
+        --primary-color: #F57C00;
+        --secondary-color: #FFB886;
+
+        --filter-color: #FFB162;
+
+        --grey-0: #000000;
+        --grey-1: #737373;
+
+        --white-0: #ffffff;
+    }
+
     * {
         margin: 0;
         padding: 0;
-
         box-sizing: border-box;
-
     }
 
-`
+    ul, ol, li {
+        list-style-type: none;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+`;
