30623b5cd12f9bd0173a8f43d92eb3221981f391
Lara <poncianolaralc@gmail.com>
2022-07-06 11:49:07 -0300

    feat: general routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Wade from './components/Wade';
 import GlobalStyle from "./styles/GlobalStyle";
+import { AppRoutes } from './pages/routes';
+
 
 function App() {
   return (
     <>
+    <AppRoutes/>
     <GlobalStyle/>
     <Wade width='6rem' height='9.5rem'/>
     </>
   );
 }
+
 
 export default App;
