54096e695e2e1495cab784a1603c647b521f8354
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-07-07 18:39:13 -0300

    feat: validation and showing error message

--- src/components/ProgressBarStep/styled.js
+++ src/components/ProgressBarStep/styled.js
@@ -11,8 +11,8 @@ export const ProgressBar = styled.div`
         position: absolute;
         top: calc(50% - 0.1rem);
         z-index: -2;
-        height: 0.2rem;
-        background-color: var(--grey-1);
+        height: 0.15rem;
+        background-color: var(--grey-yellow);
     }
 
     @media (max-width: 450px) {
@@ -21,14 +21,14 @@ export const ProgressBar = styled.div`
 `
 
 export const Step = styled.div`
-    color: ${(props) => props.currentStep >= props.stepCounter ? 'white' : 'var(--primary-color)'};
+    color: ${(props) => props.currentStep >= props.stepCounter ? 'white' : 'var(--grey-yellow)'};
     background-color: ${(props) => props.currentStep >= props.stepCounter ?'var(--primary-color)' : 'white'};
     padding: 0.1rem 0;
     width: 1.4rem;
     display: flex;
     justify-content: center;
     border-radius: 50%;
-    border: 1px solid;
+    border: 1px solid ${(props) => props.currentStep >= props.stepCounter ? 'var(--primary-color)' : 'var(--grey-yellow)'};
 `
 
 export const Progress = styled.div`
@@ -36,6 +36,6 @@ export const Progress = styled.div`
     position: absolute;
     top: calc(50% - 0.1rem);
     z-index: -1;
-    height: 0.2rem;
+    height: 0.15rem;
     background-color: var(--primary-color);
 `
