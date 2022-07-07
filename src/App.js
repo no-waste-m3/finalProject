9b618e1de9d506a8f50bbd562f74110f02071278
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-07-07 15:32:24 -0300

    feat: some validations and showing address automatically by cep

--- src/App.js
+++ src/App.js
@@ -30,18 +30,19 @@ function App() {
       <AppRoutes />
       <GlobalStyle />
       
-      <Cart />
 
       
       {/* <ToastNotify/> */}
       {/* Button para testes da notificação */}
 
+      {/*<Cart />
       <Button
         typebutton="primary"
         onClick={() => notify("Olá! Que bom te ver novamente.", 3000)}
       >
         Aqui
-      </Button>
+      <Filters/>
+  </Button>*/}
 
       {/* <NavPages /> - BALANÇO NÃO FICA COM BORDER BOTTOM}
       {/* <Footer /> */}
@@ -54,7 +55,6 @@ function App() {
 
       {/* Exemplo de utilização do modal */}
       {/* <Modal width={10} height={10} padding={.1} borderRadius={.5} top={1} left={10} backColor='--grey-1'/> */}
-      <Filters/>
 
       {/* <Modal width={10} height={10} padding={.1} borderRadius={.5} top={1} left={30} backColor='--white-0'/> */}
 
