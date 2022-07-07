5d0468a7958fec5ceafc6df72acbc623f968bacd
Gustavo Costa <gustavocosta.realitydeveloper@gmail.com>
2022-07-07 13:03:05 -0400

    feature: criado providers de Cart, Formulario para login/register e Produtos .. Ainda nao foi testado e provavelmente esta so com o esqueleto base, falta decidir como vai fazer para mostrar os erros e o que vai fazer com a resposta da api

---
+++ src/providers/Cart/index.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from "react";
+import { api } from "../../services/api";
+import { FormContext } from "../Form";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const { user, userToken } = useContext(FormContext);
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${userToken}`,
+    },
+  };
+
+  const getCart = () => {
+    api
+      .get(`/users/${user.id}/cart`, config)
+      .then((response) => setCart(response))
+      .catch((error) => console.log(error));
+  };
+
+  const postCart = (product) => {
+    api
+      .post(`/users/${user.id}/cart`, product, config)
+      .then((response) => {
+        console.log(response);
+        getCart();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const deleteCart = (id_product) => {
+    api
+      .delete(`/foods/${id_product}`, config)
+      .then((response) => {
+        console.log(response);
+        getCart();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, getCart, postCart, deleteCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
