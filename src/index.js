6fc65ffd2068d8c3cd887b857f1ab23ea83a3838
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-07-07 18:48:05 -0300

    feat: merge conflits

--- src/index.js
+++ src/index.js
@@ -6,15 +6,9 @@ import { Providers } from "./providers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-=======
   <BrowserRouter>
     <Providers>
       <App />
     </Providers>
   </BrowserRouter>
->>>>>>> 858ef2450ce18e3c1c06ada00e862fa593d9b593
 );
