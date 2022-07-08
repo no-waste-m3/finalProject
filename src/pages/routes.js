c9d9c5c1943bc9d0e838e4f97f3b749ea320481b
Lara <poncianolaralc@gmail.com>
2022-07-08 03:36:10 -0300

    About Us page

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -15,27 +15,13 @@ export const AppRoutes = () => {
     return (
 
         <Routes>
-<<<<<<< HEAD
             <Route index element={<Login />} />
             <Route path="home" element={<Home/>}/>
-            <Route path="dashboard" component={<Dashboard />}/>
-            <Route path="stats" element={<Statistics/>}/>
-            <Route path="checkout" element={<Checkout/>}/>
-            <Route path="about" element={<AboutUs/>}/>
+            <Route path="home/dashboard" component={<Dashboard />}/>
+            <Route path="dashboard/stats" element={<Statistics/>}/>
+            <Route path="home/checkout" element={<Checkout/>}/>
+            <Route path="home/about" element={<AboutUs/>}/>
             <Route path="signUp" element={<SignUp/>}/>
-=======
-            <Route path="signUp" element={<SignUp/>}/>
-            <Route path="/" element={<Login />}/>
-
-                <Route path="home" element={<Home/>}>
-                        <Route path="dashboard" element={<Dashboard/>}>
-                                <Route path="stats" element={<Statistics/>}/>
-                        </Route>
-                        <Route path="checkout" element={<Checkout/>}/>
-                        <Route path="about" element={<AboutUs/>}/>
-                </Route>
-                
->>>>>>> d4bfa89b75d68f5bec07b48f1469cd4736a9de04
         </Routes>
 
     )
