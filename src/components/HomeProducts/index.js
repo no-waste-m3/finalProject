89cbc1923fde79268f36d08ea6b39327ee969362
GusBTB <gustavocs81@gmail.com>
2022-07-11 13:02:30 -0300

    feat: filtros search bar e categorias, fix do carto button mobile

--- src/components/HomeProducts/index.js
+++ src/components/HomeProducts/index.js
@@ -3,17 +3,23 @@ import paes from "../../assets/paes.jpg";
 import { HomeProduct } from "../HomeProduct";
 import { ProductsContext } from "../../providers/Products";
 import { useContext } from "react";
+import { ProductsFiltersContext } from "../../providers/ProductsFilters";
 
 const HomeProducts = () => {
   const { products, productsUser, getProducts } = useContext(ProductsContext);
-  getProducts()
-  //console.log(products);
+  const { filteredProducts } = useContext(ProductsFiltersContext);
+  getProducts();
+  // console.log(products);
 
   return (
     <Container>
-      {products.map((product) => {
-        return <HomeProduct key={product.id} product={product} />;
-      })}
+      {filteredProducts.length > 0
+        ? filteredProducts.map((product) => {
+            return <HomeProduct key={product.id} product={product} />;
+          })
+        : products.map((product) => {
+            return <HomeProduct key={product.id} product={product} />;
+          })}
     </Container>
   );
 };
--- src/providers/index.js
+++ src/providers/index.js
@@ -1,18 +1,20 @@
 import { CartProvider } from "./Cart";
 import { FormProvider } from "./Form";
 import { ProductsProvider } from "./Products";
+import { ProductsFiltersProvider } from "./ProductsFilters";
 import { ThemeProvider } from "./Theme";
 import { ThemeToastProvider } from "./ThemeToast";
-
 
 export const Providers = ({ children }) => {
   return (
     <ThemeToastProvider>
       <FormProvider>
         <ProductsProvider>
-          <ThemeProvider>
-          <CartProvider>{children}</CartProvider>
-          </ThemeProvider>
+          <ProductsFiltersProvider>
+            <ThemeProvider>
+              <CartProvider>{children}</CartProvider>
+            </ThemeProvider>
+          </ProductsFiltersProvider>
         </ProductsProvider>
       </FormProvider>
     </ThemeToastProvider>
