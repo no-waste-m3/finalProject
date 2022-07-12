5c208bdd3ba094d710148c4734738b540cf146b8
GusBTB <gustavocs81@gmail.com>
2022-07-12 14:32:10 -0300

    fix: wade notify quando adiciona e retira do carrinho

--- src/providers/Cart/index.js
+++ src/providers/Cart/index.js
@@ -1,10 +1,14 @@
 import { createContext, useContext, useState } from "react";
 import { api } from "../../services/api";
 import { FormContext } from "../Form";
+import { notify } from "../../components/Toasts";
+import { ThemeToastContext } from "../ThemeToast";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  const { changeThemeToast } = useContext(ThemeToastContext);
+
   const [cart, setCart] = useState([]);
 
   const { user, userToken } = useContext(FormContext);
@@ -30,6 +34,10 @@ export const CartProvider = ({ children }) => {
         console.log(response);
         setCart([...cart, response.data]);
       })
+      .then((response) => {
+        changeThemeToast("success");
+        notify("Produto adicionado ao carrinho", 3000);
+      })
       .catch((error) => console.log(error));
   };
 
@@ -40,6 +48,10 @@ export const CartProvider = ({ children }) => {
         console.log(response);
         const newCart = cart.filter((item) => item.id !== id_product);
         setCart([...newCart]);
+      })
+      .then((response) => {
+        changeThemeToast("error");
+        notify("Produto retirado do carrinho", 3000, "error");
       })
       .catch((error) => console.log(error));
   };
