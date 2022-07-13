4b44355bbb23b0054f6493808885769e7b1822d6
tsunodajapa <gabrieltsunoda@gmail.com>
2022-07-13 14:56:40 -0300

    feat: corrigindo conflitos dev -> main

--- src/components/Wade/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const WadeStyled = styled.div`
-    width: ${(props) => props.width ? props.width : '7rem' };
-    height: ${(props) => props.height ? props.height : '10rem' };
-
-`
